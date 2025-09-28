aa10f67198223e89182d2a2fe20f727b47f0785c
vfhghhfy <tt8211099@gmail.com>
2025-09-28 20:27:54 +0300

    Update descargas-playlist.js

--- plugins/descargas-playlist.js
+++ plugins/descargas-playlist.js
@@ -1,28 +1,28 @@
 import yts from 'yt-search';
 
 let handler = async (m, { conn, usedPrefix, text, args, command }) => {
-if (!text) return m.reply(`*¿Qué está buscando?* Ingrese el nombre del tema\n*• Ejemplo*\n*${usedPrefix + command}* bad bunny`);
+if (!text) return m.reply(`*ماذا تبحث؟* أدخل اسم الأغنية\n*• مثال*\n*${usedPrefix + command}* bad bunny`);
 m.react('📀');
 let result = await yts(text);
 let ytres = result.videos;
-if (!ytres.length) return m.reply('❌ No se encontraron resultados.');
-let textoo = `*• Resultados de:*  ${text}\n\n`;
+if (!ytres.length) return m.reply('❌ لم يتم العثور على نتائج.');
+let textoo = `*• نتائج البحث عن:*  ${text}\n\n`;
 for (let i = 0; i < Math.min(15, ytres.length); i++) { 
 let v = ytres[i];
-textoo += `🎵 *Título:* ${v.title}\n📆 *Publicado hace:* ${v.ago}\n👀 *Vistas:* ${v.views}\n⌛ *Duración:* ${v.timestamp}\n🔗 *Enlace:* ${v.url}\n\n⊱ ────── {.⋅ ♫ ⋅.} ───── ⊰\n\n`;
+textoo += `🎵 *العنوان:* ${v.title}\n📆 *نشر منذ:* ${v.ago}\n👀 *المشاهدات:* ${v.views}\n⌛ *المدة:* ${v.timestamp}\n🔗 *الرابط:* ${v.url}\n\n⊱ ────── {.⋅ ♫ ⋅.} ───── ⊰\n\n`;
 }
 await conn.sendFile(m.chat, ytres[0].image, 'thumbnail.jpg', textoo, m);
 };
 handler.help = ['playlist', 'yts'];
 handler.tags = ['downloader'];
-handler.command = ['playvid2', 'playlist', 'playlista', 'yts', 'ytsearch'];
+handler.command = ['playvid2', 'playlist', 'اغنية', 'yts', 'اغنيه'];
 handler.register = true;
 export default handler;
 
-/*Codigo con la listas obsoleto
+/*كود مع القوائم القديم
 import yts from 'yt-search';
 let handler = async (m, { conn, usedPrefix, text, args, command }) => {
-if (!text) return m.reply(`*Que esta buscado?* ingrese el nombre del tema\n*• Ejemplo*\n*${usedPrefix + command}* bad bunny `) 
+if (!text) return m.reply(`*ماذا تبحث؟* أدخل اسم الأغنية\n*• مثال*\n*${usedPrefix + command}* bad bunny `) 
 m.react('📀');
     
 let result = await yts(text);
@@ -31,15 +31,15 @@ let listSections = [];
 for (let index in ytres) {
 let v = ytres[index];
 listSections.push({title: `${index} | ${v.title}`,
-rows: [{header: '• • •「 🅐🅤🅓🅘🅞 」• • •', title: "", description: `▢ ⌚ Duración:* ${v.timestamp}\n▢ 👀 *Vistas:* ${v.views}\n▢ 📌 *Publicado* : ${v.title}\n▢ 📆 *Subidos:* ${v.ago}\n`, id: `${usedPrefix}fgmp3 ${v.url}`
+rows: [{header: '• • •「 🅐🅤🅓🅘🅞 」• • •', title: "", description: `▢ ⌚ المدة:* ${v.timestamp}\n▢ 👀 *المشاهدات:* ${v.views}\n▢ 📌 *العنوان* : ${v.title}\n▢ 📆 *نشر منذ:* ${v.ago}\n`, id: `${usedPrefix}fgmp3 ${v.url}`
 }, {
-header: "• • •「 🅥🅘🅓🅔🅞 」• • •", title: "" , description: `▢ ⌚ Duración:* ${v.timestamp}\n▢ 👀 *Vistas:* ${v.views}\n▢ 📌 *Publicado* : ${v.title}\n▢ 📆 *Subidos:* ${v.ago}\n`, id: `${usedPrefix}fgmp4 ${v.url}`
+header: "• • •「 🅥🅘🅓🅔🅞 」• • •", title: "" , description: `▢ ⌚ المدة:* ${v.timestamp}\n▢ 👀 *المشاهدات:* ${v.views}\n▢ 📌 *العنوان* : ${v.title}\n▢ 📆 *نشر منذ:* ${v.ago}\n`, id: `${usedPrefix}fgmp4 ${v.url}`
 }, {
-header: "• • •「 🅓🅞🅒🅤🅜🅔🅝🅣🅞🅢 🅜🅟❸ 」• • •", title: "" , description: `▢ ⌚ Duración:* ${v.timestamp}\n▢ 👀 *Vistas:* ${v.views}\n▢ 📌 *Publicado* : ${v.title}\n▢ 📆 *Subidos:* ${v.ago}\n`, id: `${usedPrefix}ytmp3doc ${v.url}` }, {
-header: "'• • •「 🅓🅞🅒🅤🅜🅔🅝🅣🅞🅢 🅜🅟❹ 」• • •", title: "" , description: `▢ ⌚ Duración:* ${v.timestamp}\n▢ 👀 *Vistas:* ${v.views}\n▢ 📌 *Publicado* : ${v.title}\n▢ 📆 *Subidos:* ${v.ago}\n`, id: `${usedPrefix}ytmp4doc ${v.url}`
+header: "• • •「 🅓🅞🅒🅤🅜🅔🅝🅣🅞🅢 🅜🅟❸ 」• • •", title: "" , description: `▢ ⌚ المدة:* ${v.timestamp}\n▢ 👀 *المشاهدات:* ${v.views}\n▢ 📌 *العنوان* : ${v.title}\n▢ 📆 *نشر منذ:* ${v.ago}\n`, id: `${usedPrefix}ytmp3doc ${v.url}` }, {
+header: "'• • •「 🅓🅞🅒🅤🅜🅔🅝🅣🅞🅢 🅜🅟❹ 」• • •", title: "" , description: `▢ ⌚ المدة:* ${v.timestamp}\n▢ 👀 *المشاهدات:* ${v.views}\n▢ 📌 *العنوان* : ${v.title}\n▢ 📆 *نشر منذ:* ${v.ago}\n`, id: `${usedPrefix}ytmp4doc ${v.url}`
 }]});}
     
-await conn.sendList(m.chat, `*• Resultados:* ${text}*\n\n> *ᴇʟɪᴊᴀ ᴀ ᴜɴᴀ ᴏᴘᴄɪᴏɴ ʏ ᴘʀᴇsɪᴏɴᴇ ᴇɴᴠɪᴀʀ*`, wm, `🚀 𝙍𝙀𝙎𝙐𝙇𝙏𝘼𝘿𝙊𝙎 🚀`, ytres[0].image, listSections, m);
+await conn.sendList(m.chat, `*• النتائج:* ${text}*\n\n> *اختر خيارًا واضغط إرسال*`, wm, `🚀 النتائج 🚀`, ytres[0].image, listSections, m);
 };
 handler.help = ['playlist', 'yts']
 handler.tags = ['downloader']
