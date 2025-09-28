5a20198c6fb14714068df322be04a0a00e0bdbb8
vfhghhfy <tt8211099@gmail.com>
2025-09-28 14:31:10 +0300

    Update grupo-kick.js

--- plugins/grupo-kick.js
+++ plugins/grupo-kick.js
@@ -1,12 +1,12 @@
 let handler = async (m, { conn, participants, usedPrefix, command, isOwner }) => {
-let kickte = `A quien eliminó? etiquetas a una persona con @tag pendejo`
+let kickte = `لمن تريد الطرد؟ قم بتحديد شخص بوسم @منشن`
 if (!m.mentionedJid[0] && !m.quoted) return m.reply(kickte, m.chat, { mentions: conn.parseMention(kickte)}) 
 let user = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted.sender
 let owr = m.chat.split`-`[0]
 await conn.groupParticipantsUpdate(m.chat, [user], 'remove')}
 handler.help = ['kick *@user*']
 handler.tags = ['group']
-handler.command = ['kick', 'expulsar'] 
+handler.command = ['kick', 'expulsar', 'طرد', 'اخرج'] 
 handler.admin = true
 handler.group = true
 handler.botAdmin = true
