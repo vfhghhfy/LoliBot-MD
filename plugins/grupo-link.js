5d8f2e0788d8b876ec9ec7619d43b62d93e91056
vfhghhfy <tt8211099@gmail.com>
2025-09-29 23:01:23 +0300

    Update grupo-link.js

--- plugins/grupo-link.js
+++ plugins/grupo-link.js
@@ -3,9 +3,11 @@ const handler = async (m, {conn, args}) => {
 const group = m.chat;
 m.reply('https://chat.whatsapp.com/' + await conn.groupInviteCode(group)) 
 };
-handler.help = ['linkgroup'];
-handler.tags = ['group'];
-handler.command = /^link(gro?up)?$/i;
+
+// الأوامر العربية المضافة
+handler.help = ['linkgroup', 'رابط', 'رابط_المجموعة', 'دعوة'];
+handler.tags = ['group', 'المجموعة'];
+handler.command = /^link(gro?up)?|رابط-جروب|رابط_المجموعة|لينك$/i;
 handler.group = true;
 handler.botAdmin = true;
 handler.register = true 
