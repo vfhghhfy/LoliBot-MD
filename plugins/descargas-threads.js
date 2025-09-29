8db68291deacb06371b924bce4bfb814367768a5
vfhghhfy <tt8211099@gmail.com>
2025-09-29 08:50:07 +0300

    Update descargas-threads.js

--- plugins/descargas-threads.js
+++ plugins/descargas-threads.js
@@ -2,15 +2,15 @@ import fetch from 'node-fetch'
 const userRequests = {};
 
 const handler = async (m, { conn, args, usedPrefix, command }) => {
-if (!args[0]) return m.reply(`*⚠️ ¿Qué estás buscando? Ingresa el link de algún video de Threads!!*\n*• Ejemplo:*\n${usedPrefix + command} https://www.threads.net/@adri_leclerc_/post/C_dSNIOOlpy?xmt=AQGzxbmyveDB91QgFo_KQWzqL6PT2yCy2eg8BkhPTO-6Kw`)
+if (!args[0]) return m.reply(`*⚠️ ماذا تبحث؟ أدخل رابط فيديو من Threads!!*\n*• مثال:*\n${usedPrefix + command} https://www.threads.net/@adri_leclerc_/post/C_dSNIOOlpy?xmt=AQGzxbmyveDB91QgFo_KQWzqL6PT2yCy2eg8BkhPTO-6Kw`)
 
-if (userRequests[m.sender]) return await conn.reply(m.chat, `⏳ Hey @${m.sender.split('@')[0]} pendejo, ya hay una solicitud en proceso. Por favor, espera a que termine antes de hacer otra`, userRequests[m.sender].message || m)   
-const { key } = await conn.sendMessage(m.chat, {text: `⌛ 𝙀𝙨𝙥𝙚𝙧𝙚 ✋\n▰▰▰▱▱▱▱▱▱`}, {quoted: m}); 
+if (userRequests[m.sender]) return await conn.reply(m.chat, `⏳ يا @${m.sender.split('@')[0]} أحمق، هناك طلب قيد المعالجة بالفعل. من فضلك، انتظر حتى ينتهي قبل تقديم طلب آخر`, userRequests[m.sender].message || m)   
+const { key } = await conn.sendMessage(m.chat, {text: `⌛ انتظر ✋\n▰▰▰▱▱▱▱▱▱`}, {quoted: m}); 
 userRequests[m.sender] = { active: true, message: { key, chat: m.chat, fromMe: true } };
 await delay(1000);
-await conn.sendMessage(m.chat, {text: `⌛ 𝙀𝙨𝙥𝙚𝙧𝙚 ✋ \n▰▰▰▰▰▱▱▱▱`, edit: key});
+await conn.sendMessage(m.chat, {text: `⌛ انتظر ✋ \n▰▰▰▰▰▱▱▱▱`, edit: key});
 await delay(1000);
-await conn.sendMessage(m.chat, {text: `⌛ 𝙔𝙖 𝙘𝙖𝙨𝙞 🏃‍♂️💨\n▰▰▰▰▰▰▰▱▱`, edit: key});
+await conn.sendMessage(m.chat, {text: `⌛ لقد كاد أن ينتهي 🏃‍♂️💨\n▰▰▰▰▰▰▰▱▱`, edit: key});
 m.react(`⌛`) 
 try {
 const res = await fetch(`https://api.agatz.xyz/api/threads?url=${args[0]}`);
@@ -18,13 +18,13 @@ const data = await res.json()
 const downloadUrl = data.data.image_urls[0] || data.data.video_urls[0];
 const fileType = downloadUrl.includes('.webp') || downloadUrl.includes('.jpg') || downloadUrl.includes('.png') ? 'image' : 'video';
 if (fileType === 'image') {
-await conn.sendFile(m.chat, downloadUrl, 'threads_image.jpg', '_*Aquí tienes la imagen de Threads*_', m);
+await conn.sendFile(m.chat, downloadUrl, 'threads_image.jpg', '_*ها هي الصورة من Threads*_', m);
 m.react('✅');
 } else if (fileType === 'video') {
-await conn.sendFile(m.chat, downloadUrl, 'threads_video.mp4', '_*Aquí tienes el video de Threads*_', m);
+await conn.sendFile(m.chat, downloadUrl, 'threads_video.mp4', '_*ها هو الفيديو من Threads*_', m);
 m.react('✅');
 }
-await conn.sendMessage(m.chat, {text: `✅ 𝘾𝙤𝙢𝙥𝙡𝙚𝙩𝙖𝙙𝙤\n▰▰▰▰▰▰▰▰▰`, edit: key})   
+await conn.sendMessage(m.chat, {text: `✅ اكتمل\n▰▰▰▰▰▰▰▰▰`, edit: key})   
 } catch {   
 try {
 const res2 = await fetch(`${info.apis}/download/threads?url=${args[0]}`);
@@ -33,23 +33,23 @@ if (data2.status === true && data2.data.length > 0) {
 const downloadUrl = data2.data[0].url; 
 const fileType = data2.data[0].type; 
 if (fileType === 'image') {
-await conn.sendFile(m.chat, downloadUrl, 'threads_image.jpg', '_*Aquí tienes la imagen de Threads*_', m);
+await conn.sendFile(m.chat, downloadUrl, 'threads_image.jpg', '_*ها هي الصورة من Threads*_', m);
 m.react('✅');
 } else if (fileType === 'video') {
-await conn.sendFile(m.chat, downloadUrl, 'threads_video.mp4', '_*Aquí tienes el video de Threads*_', m);
+await conn.sendFile(m.chat, downloadUrl, 'threads_video.mp4', '_*ها هو الفيديو من Threads*_', m);
 m.react('✅');
 }}
-await conn.sendMessage(m.chat, {text: `✅ 𝘾𝙤𝙢𝙥𝙡𝙚𝙩𝙖𝙙𝙤\n▰▰▰▰▰▰▰▰▰`, edit: key})   
+await conn.sendMessage(m.chat, {text: `✅ اكتمل\n▰▰▰▰▰▰▰▰▰`, edit: key})   
 } catch (e) {
 m.react(`❌`) 
-await conn.sendMessage(m.chat, {text: `\`\`\`⚠️ OCURRIO UN ERROR ⚠️\`\`\`\n\n> *Reporta el siguiente error a mi creador con el comando:* #report\n\n>>> ${e} <<<<`, edit: key})   
+await conn.sendMessage(m.chat, {text: `\`\`\`⚠️ حدث خطأ ⚠️\`\`\`\n\n> *أبلغ عن الخطأ التالي إلى مطوري باستخدام الأمر:* #report\n\n>>> ${e} <<<<`, edit: key})   
 console.log(e) 
 }} finally {
 delete userRequests[m.sender];
 }}
 handler.help = ['thread']
 handler.tags = ['downloader']
-handler.command = /^(thread|threads|threaddl)$/i;
+handler.command = /^(thread|threads|ثريد)$/i;
 handler.register = true;
 handler.limit = 1
 
