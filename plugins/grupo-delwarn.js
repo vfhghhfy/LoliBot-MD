2be7618ad1179b8f1f46b30bc49403290d96c687
vfhghhfy <tt8211099@gmail.com>
2025-09-29 22:00:03 +0300

    Update grupo-delwarn.js

--- plugins/grupo-delwarn.js
+++ plugins/grupo-delwarn.js
@@ -9,9 +9,9 @@ who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : false
 who = m.chat;
 }
 
-if (!who) return m.reply(`*¿A quién le quito una advertencia?* Etiqueta a una persona con @tag o cita su mensaje, ¡no soy adivino! :)`)
+if (!who) return m.reply(`*لمن تريد إزالة التحذير؟* ضع علامة على شخص باستخدام @ أو رد على رسالته، لست عرافاً! :)`)
 const userResult = await db.query(`SELECT * FROM usuarios WHERE id = $1`, [who]);
-if (!userResult.rows.length) return m.reply(`*¿A quién le quito una advertencia?* Etiqueta a una persona con @tag o cita su mensaje, ¡no soy adivino! :)`)
+if (!userResult.rows.length) return m.reply(`*لمن تريد إزالة التحذير؟* ضع علامة على شخص باستخدام @ أو رد على رسالته، لست عرافاً! :)`)
 let warn = userResult.rows[0].warn || 0;
 
 if (warn > 0) {
@@ -19,14 +19,16 @@ await db.query(`UPDATE usuarios
         SET warn = warn - 1
         WHERE id = $1`, [who]);
 warn -= 1; 
-await conn.reply(m.chat, `*⚠️ SE QUITÓ UNA ADVERTENCIA ⚠️*\n\nUsuario: @${who.split`@`[0]}\n*• Advertencia:* -1\n*• Total:* ${warn}`, m)
+await conn.reply(m.chat, `*⚠️ تم إزالة تحذير ⚠️*\n\nالمستخدم: @${who.split`@`[0]}\n*• التحذير:* -1\n*• الإجمالي:* ${warn}`, m)
 } else {
-await conn.reply(m.chat, `*⚠️ El usuario @${who.split`@`[0]} no tiene ninguna advertencia.*`, m)
+await conn.reply(m.chat, `*⚠️ المستخدم @${who.split`@`[0]} ليس لديه أي تحذيرات.*`, m)
 }} catch (err) {
 }};
-handler.help = ['delwarn @user', 'unwarn @user'];
-handler.tags = ['group'];
-handler.command = /^(delwarn|unwarn)$/i;
+
+// الأوامر العربية المضافة
+handler.help = ['delwarn @user', 'unwarn @user', 'ازالة_تحذير @مستخدم', 'حذف_تحذير @مستخدم'];
+handler.tags = ['group', 'المجموعة'];
+handler.command = /^(delwarn|unwarn|ازالة_تحذير|حذف_تحذير)$/i;
 handler.group = true;
 handler.admin = true;
 handler.botAdmin = true;
