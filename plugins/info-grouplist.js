5e1b8f668872a1779966c510e1f019749cbd845b
vfhghhfy <tt8211099@gmail.com>
2025-10-01 08:41:26 +0300

    Update info-grouplist.js

--- plugins/info-grouplist.js
+++ plugins/info-grouplist.js
@@ -7,7 +7,7 @@ try {
 const res = await db.query(`SELECT id FROM chats
       WHERE is_group = true AND joined = true AND bot_id = $1`, [botId]);
 const grupos = res.rows;
-if (grupos.length === 0) return m.reply('❌ Este bot no está unido a ningún grupo.');
+if (grupos.length === 0) return m.reply('❌ هذا البوت غير مضاف إلى أي مجموعة.');
 
 for (let i = 0; i < grupos.length; i++) {
 const jid = grupos[i].id;
@@ -17,23 +17,23 @@ const botJid = conn.user?.id?.replace(/:\d+/, '');
 const bot = metadata.participants.find(u => u.id === botJid) || {};
 const isBotAdmin = bot?.admin === 'admin' || bot?.admin === 'superadmin';
 const isParticipant = metadata.participants.some(u => u.id === botJid);
-const participantStatus = isParticipant ? '✅ *Estoy aquí*' : '❌ *No estoy aquí*';
+const participantStatus = isParticipant ? '✅ *موجود*' : '❌ *غير موجود*';
 
-let link = '❌ No soy admin';
+let link = '❌ لست مشرف';
 if (isBotAdmin) {
 const code = await conn.groupInviteCode(jid).catch(() => null);
 if (code) link = `https://chat.whatsapp.com/${code}`;
-else link = '⚠️ Error al generar link';
+else link = '⚠️ خطأ في إنشاء الرابط';
 }
 
-txt += `${i + 1}. ${metadata.subject || 'Sin nombre'} | ${participantStatus}\n- *ID:* ${jid}\n- *Admin:* ${isBotAdmin ? 'Sí' : 'No'}\n- *Participantes:* ${metadata.participants.length}\n- *Link:* ${link}\n\n━━━━━━━━━━━━━━━\n\n`;
+txt += `${i + 1}. ${metadata.subject || 'بدون اسم'} | ${participantStatus}\n- *المعرف:* ${jid}\n- *مشرف:* ${isBotAdmin ? 'نعم' : 'لا'}\n- *الأعضاء:* ${metadata.participants.length}\n- *الرابط:* ${link}\n\n━━━━━━━━━━━━━━━\n\n`;
 }
-m.reply(`_*\`ESTÁ EN ESTOS GRUPOS:\`*_\n> *• Total grupo:* ${grupos.length}\n\n${txt}`.trim());
+m.reply(`_*\`المجموعات المضاف فيها:\`*_\n> *• الإجمالي:* ${grupos.length}\n\n${txt}`.trim());
 } catch (err) {
 console.error(err);
 }};
-handler.help = ['groups', 'grouplist'];
+handler.help = ['مجموعات', 'groups', 'grouplist'];
 handler.tags = ['main'];
-handler.command = /^(groups|grouplist|listadegrupo|gruposlista|listagrupos|listadegrupos|grupolista|listagrupo)$/i;
+handler.command = /^(groups|grouplist|مجموعات|قروب|جروب|قروبات)$/i;
 handler.register = true;
 export default handler;
