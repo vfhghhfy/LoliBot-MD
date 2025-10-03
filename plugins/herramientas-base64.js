d905abc39dd1e30b111b3aad256cf4b7e555f80c
vfhghhfy <tt8211099@gmail.com>
2025-10-03 23:38:37 +0300

    Update herramientas-base64.js

--- plugins/herramientas-base64.js
+++ plugins/herramientas-base64.js
@@ -1,17 +1,17 @@
 const handler = async (m, { text, usedPrefix, command }) => {
-  if (!text) return m.reply(`✳️ Usa:\n${usedPrefix + command} texto`);
+  if (!text) return m.reply(`✳️ الاستعمال:\n${usedPrefix + command} النص`);
 
   try {
     const base64 = Buffer.from(text, 'utf-8').toString('base64');
-    return m.reply(`${base64}`);
+    return m.reply(`📦 النتيجة بالـ Base64:\n\n${base64}`);
   } catch (e) {
-    return m.reply(`❌ Error al convertir: ${e.message}`);
+    return m.reply(`❌ حدث خطأ أثناء التحويل:\n${e.message}`);
   }
 };
 
-handler.help = ['tobase64']
+handler.help = ['tobase64', 'الى64', 'تحويل64']
 handler.tags = ['tools']
-handler.command = ['tobase64']
+handler.command = /^(tobase64|الى64|تحويل64)$/i
 handler.register = true
 handler.limit = 1
 
