101a183439eeef034a859e91c96abd58009bdbce
vfhghhfy <tt8211099@gmail.com>
2025-10-03 23:37:33 +0300

    Update herramienta-id.js

--- plugins/herramienta-id.js
+++ plugins/herramienta-id.js
@@ -1,10 +1,20 @@
 let handler = async (m, { conn, text, isOwner }) => {
-let USER_ID = m.user.lid 
-conn.fakeReply(m.chat, USER_ID, '0@s.whatsapp.net', `👇 AQUI ESTA TU NUMERO OCULTO "LID" 👇`, 'status@broadcast')
-//m.reply(USER_ID)
+  let USER_ID = m.user.lid 
+  
+  // رد وهمي لإظهار المعرف (LID)
+  conn.fakeReply(
+    m.chat, 
+    USER_ID, 
+    '0@s.whatsapp.net', 
+    `👇 *هنا رقمك المخفي (LID)* 👇\n\n🔑 ${USER_ID}`, 
+    'status@broadcast'
+  )
+  // أو لو تبي رد عادي بدل الوهمي:
+  // m.reply(`🔑 رقمك المخفي (LID): ${USER_ID}`)
 }
-handler.help = ['mylid'];
-handler.tags = ['tools'];
-handler.command = /^mylid$/i;
 
-export default handler;
+handler.help = ['mylid', 'معرفي', 'لِد']
+handler.tags = ['tools']
+handler.command = /^(mylid|معرفي|لِد)$/i  // يدعم أوامر بالعربي والإنجليزي
+
+export default handler
