8c9de9750b44ad41b739296cf1aa39a6b2a727a4
vfhghhfy <tt8211099@gmail.com>
2025-10-03 23:50:25 +0300

    Update owner-autoadmin.js

--- plugins/owner-autoadmin.js
+++ plugins/owner-autoadmin.js
@@ -1,10 +1,17 @@
 let handler = async (m, { conn, isAdmin }) => {
-if (m.fromMe) throw 'Nggk'
-if (isAdmin) return m.reply('Ya eres admin del grupo mi creador 🫡')
-await conn.groupParticipantsUpdate(m.chat, [m.sender], "promote")}
-handler.help = ['autoadmin']
+  if (m.fromMe) throw '🚫 لا يمكنك usar este comando desde el propio bot.'; 
+
+  if (isAdmin) 
+    return m.reply('✅ Ya eres admin del grupo mi creador 🫡\n✅ أنت بالفعل أدمن في المجموعة يا صانعـي 🫡');
+
+  await conn.groupParticipantsUpdate(m.chat, [m.sender], "promote");
+  return m.reply('✅ Ahora eres administrador.\n✅ تم ترقيتك إلى أدمن.');
+};
+
+handler.help = ['autoadmin', 'ترقية']
 handler.tags = ['owner']
-handler.command = /^admin.|atad|autoadmin$/i
+handler.command = /^(autoadmin|admin\.|atad|ترقية|رفع)$/i
 handler.owner = true
 handler.botAdmin = true
-export default handler
+
+export default handler;
