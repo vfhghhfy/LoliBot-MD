d27006b2b97de905dff5ea96f7934882297c8b98
vfhghhfy <tt8211099@gmail.com>
2025-10-05 14:32:38 +0300

    Update config.js

--- config.js
+++ config.js
@@ -18,7 +18,7 @@ global.owner = [
 globalThis.info = {
 wm: "𝙇𝙤𝙡𝙞𝘽𝙤𝙩",
 vs: "2.0.0 (beta)",
-packname: "𝗦𝗧𝗜𝗖𝗞𝗘𝗥𝗦❤️‍🔥 - LoliBot\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n",
+packname: "𝕃𝕆𝕃𝕀𝔹𝕆𝕋",
 author: "Owner: @elrebelde21\n• Dueña: @itschinita_official",
 apis: "https://api.delirius.store",
 apikey: "GataDios",
@@ -26,19 +26,19 @@ fgmods: { url: 'https://api.fgmods.xyz/api', key: 'elrebelde21' },
 neoxr: { url: 'https://api.neoxr.eu/api', key: 'GataDios' },
 img2: "https://files.catbox.moe/nz2421.jpg",
 img4: fs.readFileSync('./media/Menu2.jpg'),
-yt: "https://www.youtube.com/@elrebelde.21",
-tiktok: "https://www.tiktok.com/@elrebelde.21",
+yt: "https://www.youtube.com/@elrebe",
+tiktok: "https://www.tiktok.com/@elrebe",
 md: "https://github.com/elrebelde21/LoliBot-MD",
-fb: "https://www.facebook.com/elrebelde21",
-ig: "https://www.instagram.com/its_china_official",
-nn: "https://chat.whatsapp.com/HNDVUxHphPzG3cJHIwCaX5", //Grupo ofc1
-nn2: "https://chat.whatsapp.com/KDBt6S54riRCIpSZspkxhg", //Grupo ofc2
-nn3: "https://chat.whatsapp.com/GXNXKWSEckU1j1c7sItTxK", //Colab Loli & Gata
-nn4: "https://chat.whatsapp.com/Em4Byf4w5VgHObPvZQlfnM", //Enlace LoliBot
-nn5: "https://chat.whatsapp.com/J5hQvECqR4W6uTT84uiSBx", //A.T.M.M
-nn6: "https://chat.whatsapp.com/ILAHJeZsHh973tQ96i2aqS", //Dev support 
-nna: "https://whatsapp.com/channel/0029Vah0NnV6mYPDQI7bpt0z",
-nna2: "https://whatsapp.com/channel/0029Vah0NnV6mYPDQI7bpt0z"
+fb: "https://www.facebook.com/elreb",
+ig: "https://www.instagram.com/j_5f4?igsh=bXp6dXdiOWNkY2w0",
+nn: "https://whatsapp.com/channel/0029VapxAcz4SpkEe0XN6X3V", //Grupo ofc1
+nn2: "https://whatsapp.com/channel/0029VapxAcz4SpkEe0XN6X3V", //Grupo ofc2
+nn3: "https://whatsapp.com/channel/0029VapxAcz4SpkEe0XN6X3V", //Colab Loli & Gata
+nn4: "https://whatsapp.com/channel/0029VapxAcz4SpkEe0XN6X3V", //Enlace LoliBot
+nn5: "https://whatsapp.com/channel/0029VapxAcz4SpkEe0XN6X3V", //A.T.M.M
+nn6: "https://whatsapp.com/channel/0029VapxAcz4SpkEe0XN6X3V", //Dev support 
+nna: "https://whatsapp.com/channel/0029VapxAcz4SpkEe0XN6X3V",
+nna2: "https://whatsapp.com/channel/0029VapxAcz4SpkEe0XN6X3V"
 }
 
 //----------------------------------------------------
